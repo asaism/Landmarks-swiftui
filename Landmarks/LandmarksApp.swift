45cce4f3b6b6c5ae12662bbb435a10dc78971c17
Suguru ASAI <suguru@asaism.com>
2022-01-08 18:26:10 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Suguru ASAI on 2022/01/08.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
